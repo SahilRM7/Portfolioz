d5940c2664a2a2944b759364763717ace9266e58
sahil <118128555+KingSahil07@users.noreply.github.com>
2025-08-12 14:25:21 +0530

    Update urls.py

--- portfolioz/urls.py
+++ portfolioz/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     # path('admin/', admin.site.urls),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
